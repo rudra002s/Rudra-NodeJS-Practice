1f2912648b472466979d7eb2adf7adb17f0bd243
Rudra Sharma <rudras.1902@gmail.com>
2026-03-20 17:42:51 +0530

    Add express dependency and implement basic server functionality

--- Backend-Node.js-npm/server.js
+++ Backend-Node.js-npm/server.js
@@ -1,1 +1,7 @@
-console.log("Hello world")
+var slugify = require('slugify')
+
+let a=slugify('some string') // some-string
+console.log(a)
+// if you prefer something other than '-' as separator
+const b=slugify('some string', '_')  // some_string
+console.log(b)
