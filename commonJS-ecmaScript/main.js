ef47da885da66a9aa0d7fff50cf7cd1ef38ed48c
Rudra Sharma <rudras.1902@gmail.com>
2026-03-21 20:13:33 +0530

    refactor: update main.js to use ES modules and add mymodule.js

--- commonJS-ecmaScript/main.js
+++ commonJS-ecmaScript/main.js
@@ -1,15 +1,2 @@
-const { createServer } = require('node:http');
-const fs=require("fs")
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1> Hello World </h1>');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-}); 
+import {a} from "./mymodule.js"
+console.log(a)
