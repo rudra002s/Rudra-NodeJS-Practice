4e63b8ededa2e4005f6a758f1967249ee545a4d2
rudra002s <rudras.1902@gmail.com>
2026-03-26 23:28:59 +0530

    feat: add rudra.txt and rudra2.txt with initial content and update main.js for file writing

---
+++ files-fs-pathModules/main.js
@@ -0,0 +1,10 @@
+const fs=require("fs")
+
+console.log("starting")
+//fs.writeFileSync("rudra.txt","Rudra is smart")
+//it waits until the file is written
+
+fs.writeFile("rudra2.txt","Rudra is getting smarter each day",()=>{
+    console.log("done")
+})
+console.log("ending")
