0e0c95ebad95522b15ad2d4adcd102b8b595fd9a
rudra002s <rudras.1902@gmail.com>
2026-03-28 20:43:56 +0530

    feat: add aboutPath.js for path operations and logging

---
+++ files-fs-pathModules/aboutPath.js
@@ -0,0 +1,7 @@
+import path from "path"
+
+let myPath="/Users/apple/Downloads/RUDRA/WEB DEV/NodeJs/files-fs-pathModules/rudra2.txt"
+
+console.log(path.extname(myPath))
+console.group(path.dirname(myPath))
+console.group(path.basename(myPath))
